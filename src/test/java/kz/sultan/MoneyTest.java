c9a908cb716c04d1d2273fa11b40dc2634a6ad9d
Sultan <sultansakyp77@gmail.com>
2025-09-30 10:54:45 +0500

    Test-Driven-Development by Example book

--- src/test/java/kz/sultan/MoneyTest.java
+++ src/test/java/kz/sultan/MoneyTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
+    // By Test-Driven-Development by Example book
     @Test
     void testMultiplication() {
         Money fiveDollar  = Money.dollar(5);
